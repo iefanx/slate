cc2335e189d3e637da575d712408356f81c98541
iefan <66840141+iefanx@users.noreply.github.com>
2026-05-20 17:29:23 +0530

    Bump Android TWA appVersionCode to 4 and version to 4.0 to resolve Google Play Console conflicts, and configure .gitignore

---
+++ slate-android/app/src/main/java/com/iefanx/slate/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.iefanx.slate.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
